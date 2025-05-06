1e7118f5e05713ae14190fa4a929f71c89d5721a
Kmiktt <kmilagato05@gmail.com>
2025-05-06 13:54:16 -0400

    Clase "Nota"

---
+++ src/main/java/Nota.java
@@ -0,0 +1,14 @@
+
+public class Nota {
+    private String contenido;
+
+    public Nota(String x){
+        contenido=x;
+    }
+    public void setNota(String x){
+        contenido=x;
+    }
+    public String getNota(){
+        return contenido;
+    }
+}
