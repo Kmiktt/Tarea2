b8374345a514f5c59c72f1d2f0412823054e3a8a
Kmiktt <kmilagato05@gmail.com>
2025-05-06 14:13:36 -0400

    Clase "Empleado"

---
+++ src/main/java/Empleado.java
@@ -0,0 +1,38 @@
+//falta por agregar implements invitable
+public class Empleado {
+    private String id;
+    private String apellidos;
+    private String nombre;
+    private String correo;
+
+    public Empleado(String x, String y, String z, String c){
+        id=x;
+        apellidos=y;
+        nombre=z;
+        correo=c;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+    //quiza dejarlo en protected, pero la opcion de cambiar correo y nombre a veces se hace necesaria
+    public void setCorreo(String x){
+        correo=x;
+    }
+
+    public void setNombre(String x){
+        nombre=x;
+    }
+
+}
