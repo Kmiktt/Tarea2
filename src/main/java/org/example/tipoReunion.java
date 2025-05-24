aebce6321401e1f4380457ef7bb8a82f8fee5cc8
Kmiktt <kmilagato05@gmail.com>
2025-05-23 21:15:31 -0400

    Javadoc para mejor comprension

--- src/main/java/org/example/tipoReunion.java
+++ src/main/java/org/example/tipoReunion.java
@@ -1,13 +1,18 @@
 package org.example;
 
+
+/**Almacenan los diferentes posibles temas de una reunion*/
 public enum tipoReunion {
     TECNICA("Tecnica"),
     MARKETING("Marketing"),
     OTRO("otro");
     private String tipo;
+    /**Constructor de enum
+     *@param a String de el tipo de Reunion */
     tipoReunion(String a){
         tipo = a;
     }
+    /**Getter de String del tipo*/
     public String getTipo(){
         return tipo;
     }
